f65075393bdc1f34791d71e6aff62060d03126b8
Rvanje <53704685+Rvanje@users.noreply.github.com>
2024-07-07 10:34:24 +0200

    - Integrating the connection_check-function into the main function

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,19 +7,18 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input = {"raw_document": {"text": text_to_analyze}}
 
-    connecton_check(requests.post(url, headers=header, json=input))
+    response = requests.post(url, headers=header, json=input)
 
-def connecton_check(response):
-    if response == range(200, 211):
-        return "Connection established.", response.json().get('emotions', {})
-    if response == range(300, 311):
-        return "Connection error."
-    if response == range(400, 411):
-        return "Client error."
-    if response == range(500, 511):
-        return "Server error."
+    if 200 <= response.status_code <= 211:
+        print("Connection established."), response.json().get('emotions', {})
+    if 300 <= response.status_code <= 311:
+        print("Connection error.")
+    if 400 <= response.status_code <= 411:
+        print("Client error.")
+    if 500 <= response.status_code <= 511:
+        print("Server error.")
     else:
-        return "Unknown error."
+        print("Unknown error.")
 
 
 if __name__ == "__main__":
